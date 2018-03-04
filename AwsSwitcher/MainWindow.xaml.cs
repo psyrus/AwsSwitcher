ba59fe4b80aced5facbb4d320ce54fa028beb458
Trevor <revolution.x@gmail.com>
2018-03-04 22:23:06 +0900

    Added settings window and linking of OVPN file

--- AwsSwitcher/MainWindow.xaml.cs
+++ AwsSwitcher/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Xml.Linq;
 using System.Linq;
 using Amazon.EC2.Model;
 using System.IO;
+using Microsoft.Win32;
 
 namespace AwsSwitcher
 {
@@ -20,7 +21,7 @@ namespace AwsSwitcher
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string CONFIG_FILE = "aws_config.xml";
+        public string CONFIG_FILE = "aws_config.xml";
         private DispatcherTimer dispatcherTimer = new DispatcherTimer();
         private List<Task> runningTasks = new List<Task>();
         private Dictionary<string, string> settings;
@@ -36,49 +37,11 @@ namespace AwsSwitcher
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
             dispatcherTimer.Start();
-        }
-        void ShowHideDetails(object sender, RoutedEventArgs e)
-        {
-            for (var vis = sender as Visual; vis != null; vis = VisualTreeHelper.GetParent(vis) as Visual)
-                if (vis is DataGridRow)
-                {
-                    var row = (DataGridRow)vis;
-                    row.DetailsVisibility =
-                    row.DetailsVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
-                    break;
-                }
+
+            //settings["testKey"] = "testVal";
+            //SaveSettings(CONFIG_FILE, settings);
         }
 
-        void ToggleInstanceState(object sender, RoutedEventArgs e)
-        {
-            if (this.targetInstance == null)
-            {
-                Console.WriteLine("Error: Instance does not exist");
-                return;
-            }
-            ActionButton.Content = "Please wait...";
-
-            using (var awsClient = GetClient())
-            { 
-                if (targetInstance.State.Code > 64)
-                {
-                    StartInstancesRequest req = new Amazon.EC2.Model.StartInstancesRequest();
-                    req.InstanceIds.Add(targetInstance.InstanceId);
-                    awsClient.StartInstances(req);
-                }
-                else
-                {
-                    StopInstancesRequest req = new Amazon.EC2.Model.StopInstancesRequest();
-                    req.InstanceIds.Add(targetInstance.InstanceId);
-                    awsClient.StopInstances(req);
-                }
-            }
-            UpdateUI();
-        }
-
-        /// <summary>
-        /// This function will update the UI periodically to check if a button needs to be changed
-        /// </summary>
         void UpdateUI()
         {
             dispatcherTimer.Start();
@@ -97,7 +60,7 @@ namespace AwsSwitcher
                     isChangingState = true;
                 }
             }
-            
+
             if (!isChangingState)
             {
                 dispatcherTimer.Stop();
@@ -107,44 +70,12 @@ namespace AwsSwitcher
             }
         }
 
-        private void ConfigureVPN(Instance targetInstance)
-        {
-            string text = File.ReadAllText(settings["vpn_config"]);
-            text = Regex.Replace(text, @"^remote ((?:\d*\.?)+) 1194", String.Format("remote {0} 1194", targetInstance.PublicIpAddress ?? "0.0.0.0") ,RegexOptions.Multiline);
-            File.WriteAllText(settings["vpn_config"], text);
-        }
-
-        private object GetInstanceAction(Instance targetInstance)
-        {
-            return targetInstance.State.Code > 64 ? "Start instance" : "Shut down instance";
-        }
-
+        #region Settings and helpers
         AmazonEC2Client GetClient()
         {
             RegionEndpoint region = RegionEndpoint.APSoutheast2;
 
             return new AmazonEC2Client(settings["access_id"], settings["access_key"], region);
-        }
-
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-
-            UpdateUI();
-            return;
-            //Check all current tasks to make sure all are complete.
-            //for (int i = 0; i < runningTasks.Count; i++)
-            //{
-            //    Task item = runningTasks[i];
-            //    if (item.IsCompleted)
-            //    {
-            //        runningTasks.Remove(item);
-            //    }
-            //    else
-            //    {
-            //        //One of the tasks has not yet finished, thus we should skip this update round.
-            //        return;
-            //    }
-            //}
         }
 
         public Dictionary<string, string> GetSettings(string path)
@@ -162,16 +93,83 @@ namespace AwsSwitcher
 
         }
 
-        private void InstanceButton_Click(object sender, RoutedEventArgs e)
+        public void SaveSettings(string path, Dictionary<string, string> settingsToSave)
         {
 
-            
+            XElement el = new XElement("root",
+                settingsToSave.Select(kv => new XElement(kv.Key, kv.Value)));
+
+            el.Save(path);
+        }
+
+        private object GetInstanceAction(Instance targetInstance)
+        {
+            return targetInstance.State.Code > 64 ? "Start instance" : "Shut down instance";
+        }
+
+        private void ConfigureVPN(Instance targetInstance)
+        {
+            string text = File.ReadAllText(settings["vpn_config"]);
+            text = Regex.Replace(text, @"^remote ((?:\d*\.?)+) 1194", String.Format("remote {0} 1194", targetInstance.PublicIpAddress ?? "0.0.0.0"), RegexOptions.Multiline);
+            File.WriteAllText(settings["vpn_config"], text);
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        void ToggleInstanceState(object sender, RoutedEventArgs e)
+        {
+            if (this.targetInstance == null)
+            {
+                Console.WriteLine("Error: Instance does not exist");
+                return;
+            }
+            ActionButton.Content = "Please wait...";
+
+            using (var awsClient = GetClient())
+            {
+                if (targetInstance.State.Code > 64)
+                {
+                    StartInstancesRequest req = new Amazon.EC2.Model.StartInstancesRequest();
+                    req.InstanceIds.Add(targetInstance.InstanceId);
+                    awsClient.StartInstances(req);
+                }
+                else
+                {
+                    StopInstancesRequest req = new Amazon.EC2.Model.StopInstancesRequest();
+                    req.InstanceIds.Add(targetInstance.InstanceId);
+                    awsClient.StopInstances(req);
+                }
+            }
+            UpdateUI();
+        }
+
+        private void InstanceButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow sw = new SettingsWindow();
+            sw.PopulateSettings(settings);
+            sw.Show();
+
         }
 
         private void VpnButton_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog ofg = new OpenFileDialog();
+            if (ofg.ShowDialog() == true)
+            {
+                settings["vpn_config"] = ofg.FileName;
+                Console.WriteLine(settings["vpn_config"]);
+            }
 
         }
+        #endregion
+
     }
 
 }
---
+++ AwsSwitcher/SettingsWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AwsSwitcher
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow parentWindow = (MainWindow)Application.Current.MainWindow;            
+            Dictionary<string, string> settings = new Dictionary<string, string>();
+
+            settings["access_id"] = access_id.Text;
+            settings["access_key"] = access_key.Text;
+            settings["instance_id"] = instance_id.Text;
+            settings["vpn_config"] = vpn_config.Text;
+
+            parentWindow.SaveSettings(parentWindow.CONFIG_FILE, settings);
+            this.Close();
+        }
+
+        internal void PopulateSettings(Dictionary<string, string> settings)
+        {
+            access_id.Text = settings["access_id"];
+            access_key.Text = settings["access_key"];
+            instance_id.Text = settings["instance_id"];
+            vpn_config.Text = settings["vpn_config"];
+        }
+    }
+}
